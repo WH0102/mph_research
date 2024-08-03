aebe460bdab51f52b5238a60af57bc84172d43b6
WH0102 <whchin0102@gmail.com>
2024-08-03 14:47:45 +0800

    add dictionary for district

--- pages/5_👩‍👩‍👦‍👦_STR_Population.py
+++ pages/5_👩‍👩‍👦‍👦_STR_Population.py
@@ -40,6 +40,10 @@ class map:
         'tealrose', 'tempo', 'temps', 'thermal', 'tropic', 'turbid',
         'turbo', 'twilight', 'viridis', 'ylgn', 'ylgnbu', 'ylorbr', 'ylorrd'
     ]
+    _dict_district = {"Cameron Highland":"Cameron Highlands",
+                      "Sp Selatan":"Seberang Perai Selatan",
+                      "Sp Tengah":"Seberang Perai Tengah",
+                      "Sp Utara":"Seberang Perai Utara"}
 
     def read_geojson_file(file:str):
         # Import packages
